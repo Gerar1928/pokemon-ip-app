44ff73edb35a8b7fe6e424d03a2938fce38b520d
Gerar1928 <Gerar1928@gmail.com>
2025-04-30 01:37:31 -0400

    testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = 80;
 
 let serverIP = 'Obteniendo IP...';
 
